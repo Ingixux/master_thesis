eb16e47e8ed1b4998f2ab1b7a2020aba5f439ecc
Ingixux <Ingebretoseland@hotmail.com>
2024-02-27 15:57:27 +0100

    missing syn to entropy and the rest of threashold

--- Entropy.py
+++ Entropy.py
@@ -13,7 +13,7 @@ class Entropy:
         self.aggregate_window={"currentTime":0,"earlistTime":0,"interval":datetime.timedelta(microseconds=aggregate_window_duration*1000),"vaules":[]}
         self.comparison_window={"currentTime":0,"currentRecs":[],"earlistTime":0,"interval":comparison_window_interval,"vaules":[]}
         for x in range(0,int(aggregate_window_duration/sliding_window_interval)):
-            self.aggregate_window["vaules"].append({"sourceIP":{},"packets":0,"destinationIP":{},"currentRecs":[]})
+            self.aggregate_window["vaules"].append({"sourceIP":{},"packets":0,"destinationIP":{},"bytes":0,"packetsize":{},"currentRecs":[],"biflow":{}})
 
         #for x in range(0,int(comparison_window_interval/sliding_window_interval)-(aggregate_window_duration/sliding_window_interval)):
         #    self.comparison_window["vaules"].append({"sourceIP":{},"packets":0,"destinationIP":{}})
@@ -34,7 +34,7 @@ class Entropy:
             self.aggregate_window["earlistTime"]=self.aggregate_window["currentTime"]
             if len(self.aggregate_window["vaules"][-1]["currentRecs"])!=0: #TODO this will not handle how I want if there in the midle comes a empty window
                 toretrun=copy.copy(self.doCalculation())
-                toretrun=self.doCalculation()
+                #toretrun=self.doCalculation()
             elif self.startcounter<len(self.aggregate_window["vaules"]):
                 self.startcounter+=1
             self.removeFromSliding_window()
@@ -45,14 +45,26 @@ class Entropy:
     def addVaules(self,rec):
         self.aggregate_window["vaules"][0]["currentRecs"].append(rec)
         self.aggregate_window["vaules"][0]["packets"]=self.aggregate_window["vaules"][0]["packets"] +rec.packets #TODO fix this
+        self.aggregate_window["vaules"][0]["bytes"]=self.aggregate_window["vaules"][0]["bytes"] +rec.bytes #TODO fix this
+
+        checkKeyOfBiFlow = str(rec.dip)+ str(rec.sip) + str(rec.dport) +str(rec.sport) +str(rec.protocol)
+        if checkKeyOfBiFlow in self.aggregate_window["vaules"][0]["biflow"].keys():
+            self.aggregate_window["vaules"][0]["biflow"][checkKeyOfBiFlow]["packets"]+=1*rec.packets
+        else:
+            keyOfBiFlow= str(rec.sip) + str(rec.dip) +str(rec.sport) + str(rec.dport) +str(rec.protocol)
+            self.aggregate_window["vaules"][0]["biflow"][keyOfBiFlow]={"packets":1*rec.packets,"checkKey":checkKeyOfBiFlow}
         if self.cheackIfNovaule("sourceIP",str(rec.sip)):
-            self.aggregate_window["vaules"][0]["sourceIP"][str(rec.sip)] =1
+            self.aggregate_window["vaules"][0]["sourceIP"][str(rec.sip)] =1*rec.packets
         else:
-            self.aggregate_window["vaules"][0]["sourceIP"][str(rec.sip)] +=1
+            self.aggregate_window["vaules"][0]["sourceIP"][str(rec.sip)] +=1*rec.packets
         if self.cheackIfNovaule("destinationIP",str(rec.dip)):
-            self.aggregate_window["vaules"][0]["destinationIP"][str(rec.dip)] =1
+            self.aggregate_window["vaules"][0]["destinationIP"][str(rec.dip)] =1*rec.packets
         else:
-            self.aggregate_window["vaules"][0]["destinationIP"][str(rec.dip)] +=1
+            self.aggregate_window["vaules"][0]["destinationIP"][str(rec.dip)] +=1*rec.packets
+        if self.cheackIfNovaule("packetsize",str(int(rec.bytes/rec.packets))):
+            self.aggregate_window["vaules"][0]["packetsize"][str(int(rec.bytes//rec.packets))] =1*rec.packets
+        else:
+            self.aggregate_window["vaules"][0]["packetsize"][str(int(rec.bytes//rec.packets))] +=1*rec.packets
 
     def cheackIfNovaule(self,valueToCheck,anothervaule =None):
         if anothervaule ==None:
@@ -75,6 +87,9 @@ class Entropy:
         totaldestinationIP=0 
         uniqetotalsourceIP={}
         uniqedestinationIP={}
+        uniqepacketsize={}
+        uniqebiflow={}
+        checkkey=[]
         for x in range(0,len(self.aggregate_window["vaules"])):
             totalpackets+=self.aggregate_window["vaules"][x]["packets"]
             for key in self.aggregate_window["vaules"][x]["sourceIP"].keys():
@@ -89,26 +104,55 @@ class Entropy:
                     uniqedestinationIP[key]+=self.aggregate_window["vaules"][x]["destinationIP"][key]
                 except KeyError:
                     uniqedestinationIP[key]=self.aggregate_window["vaules"][x]["destinationIP"][key]
+            for key in self.aggregate_window["vaules"][x]["packetsize"].keys():
+                try:
+                    uniqepacketsize[key]+=self.aggregate_window["vaules"][x]["packetsize"][key]
+                except KeyError:
+                    uniqepacketsize[key]=self.aggregate_window["vaules"][x]["packetsize"][key]
+            for key in self.aggregate_window["vaules"][x]["biflow"].keys(): 
+                try:
+                    uniqebiflow[key]+=self.aggregate_window["vaules"][x]["biflow"][key]["packets"]
+                except KeyError:
+                    try:
+                        uniqebiflow[self.aggregate_window["vaules"][x]["biflow"][key]["checkKey"]]+=self.aggregate_window["vaules"][x]["biflow"][key]["packets"]
+                    except KeyError:
+                        uniqebiflow[key]=self.aggregate_window["vaules"][x]["biflow"][key]["packets"]
+                    
+
+
+
         arrayToAdd=[]
         listofprobability=[]
         for x in uniqetotalsourceIP.values():
             listofprobability.append(x/totalpackets)
         entropySip=self.entropyCal(listofprobability)
         entropyRateSip=entropySip/len(uniqetotalsourceIP)
-        
+    
         listofprobability=[]
         for x in uniqedestinationIP.values():
             listofprobability.append(x/totalpackets)
         entropyDip=self.entropyCal(listofprobability)
-        entropyRateDip=entropyDip/len(uniqedestinationIP)
+        entropyRateDip=entropyDip/len(uniqedestinationIP)#TODO it might be worth it to look at all the size from the lowest to the highst
+        
+        listofprobability=[]
+        for x in uniqepacketsize.values():
+            listofprobability.append(x/totalpackets)
+        entropyPacketsize=self.entropyCal(listofprobability)
+        entropyRatePacketsize=entropyPacketsize/len(uniqepacketsize)
+
+        listofprobability=[]
+        for x in uniqebiflow.values():
+            listofprobability.append(x/totalpackets)
+        entropyBiflow=self.entropyCal(listofprobability)
+        entropyRateBiflowe=entropyPacketsize/len(uniqebiflow)
+
         #print(self.aggregate_window["vaules"][0]["currentRecs"])
         for rec in self.aggregate_window["vaules"][-1]["currentRecs"]:
             arrayToAdd.append([rec.stime, rec.etime, int(rec.sip), int(rec.dip), rec.sport, rec.dport, rec.protocol, rec.packets, rec.bytes, 
                                 int(rec.tcpflags.fin), int(rec.tcpflags.syn), int(rec.tcpflags.rst), int(rec.tcpflags.psh), int(rec.tcpflags.ack), 
                                 int(rec.tcpflags.urg), int(rec.tcpflags.ece), int(rec.tcpflags.cwr), rec.duration/datetime.timedelta(milliseconds=1), 
-                                entropySip,entropyRateSip,entropyDip,entropyRateDip,
+                                entropySip,entropyRateSip,entropyDip,entropyRateDip,entropyPacketsize,entropyRatePacketsize,entropyBiflow,entropyRateBiflowe,
                                 self.setIsAttack(rec)])
-          
         return arrayToAdd                 
 
     def entropyCal(self,listofprobability):
@@ -129,8 +173,8 @@ class Entropy:
         #TODO create the data to retrun from the sliding_window, can use packets in all aggregate_window to see if aggregate_window is complete
         for x in range(1,len(self.aggregate_window["vaules"])):
             self.aggregate_window["vaules"][-x]=self.aggregate_window["vaules"][-(x+1)]
-        self.aggregate_window["vaules"][0]={"sourceIP":{},"packets":0,"destinationIP":{}}
-        self.aggregate_window["vaules"][0]["currentRecs"]=[]
+        self.aggregate_window["vaules"][0]={"sourceIP":{},"packets":0,"destinationIP":{},"bytes":0,"packetsize":{},"currentRecs":[],"biflow":{}}
+        #self.aggregate_window["vaules"][0]["currentRecs"]=[]
 
     def checkIfpastedSliding_window(self):
         #print(self.aggregate_window["interval"])
